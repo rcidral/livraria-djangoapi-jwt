a8f085a1ff945680d44b92a88716a380ed469a80
rcidral <ricidralmfo@gmail.com>
2023-02-02 22:56:50 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Categoria, Editora, Autor, Livro
+from core.serializers import CategoriaSerializer, EditoraSerializer, AutorSerializer, LivroSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class EditoraViewSet(ModelViewSet):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+
+class AutorViewSet(ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class LivroViewSet(ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+
+
